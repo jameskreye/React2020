6a72120f84bb7180b8a2cbb5cfec7b85ec456e68
jamesonMichel <jameson.michel@23people.io>
2020-04-23 17:56:01 -0400

    Adding new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable react/display-name */
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <h4 class="bg-primary text-white text-center p-2 m-1">
+        Component Element
+    </h4>
+   
   );
 }
 
